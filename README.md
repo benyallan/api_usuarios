d532c1ba6bc18e4885afb879d997ddb2c9d4261b
Beny Allan <benyallan@yahoo.com.br>
2022-08-10 19:39:03 -0300

    npm install rodado

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# projeto6-formacao-nodejs
+# Testes com API
--- UsersAPI/database/connection.js
+++ UsersAPI/database/connection.js
@@ -2,9 +2,9 @@ var knex = require('knex')({
     client: 'mysql2',
     connection: {
       host : '127.0.0.1',
-      user : 'root',
-      password : '',
-      database : 'apiusers'
+      user : 'node',
+      password : 'node',
+      database : 'nodetest'
     }
   });
 
